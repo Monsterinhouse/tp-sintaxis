ee89f46a2867152d9670dcd43a66329a196b1eb5
Mosnterinhouse <lucas.cruz2003@hotmail.com>
2026-05-06 17:22:10 -0300

    desarrollo de aplicacion y perfilacion de TAD'S

--- APLICACION GESTION DE INVESTIGADORES.py
+++ APLICACION GESTION DE INVESTIGADORES.py
@@ -1,9 +1,12 @@
+from os import system
 from TADInvestigadores import*
 from datetime import datetime, time
 
 x=0
+i = []
 
 while (x==0):
+    system("cls")
     print ("##### MENU DE OPCIONES ##### \n")
     print("[1]:Cargar nuevo investigador")
     print("[2]:Ver informacion de Investigador")
@@ -19,8 +22,47 @@ while (x==0):
 
     match opcion :
         case 1 : 
-            print ("hola")
+            system("cls")
+            inv = crearInvestigador()
+            nombre = input("Ingrese su Nombre: ")
+            apellido = input("Ingrese su Apellido: ")
+            legajo = input("Ingrese su Legajo: ")
+            fechIng = input("Ingrese su Fecha de Ingreso (DD/MM/AAAA): ")
+            print (f"len:{len(fechIng)}")
+            if (fechIng.rfind("-")) :
+                fechIng = fechIng.replace("-","/")
+            elif (len(fechIng) <= 0 | len(fechIng) > 10) :
+                print ("Ingrese una fecha valida!")
+            numLab = input ("Ingrese su numero de Laboratorio: ")
+            area = input ("Ingrese su area: \n1) Area 1\n2) Area 2\n3) Area 3\nOpcion: ")
+            if (area == 1) :
+                area == 'Area1'
+            elif (area == 2) :
+                area == 'Area2'
+            elif (area == 3) :
+                area == 'Area3'
+            cargarInvestigador(inv,nombre,apellido,legajo,fechIng,numLab,area)
+            sumarInvestigador(i,inv)
+            print(f"Se ha cargado satisfactoriamente el investigador: \n{inv}")
+            input()
+        
+        case 5:
+            if (esVacia(i)) :
+                system("cls")
+                print("No hay elementos cargados en la lista!\nProba ca6rgando un Investigador con la Opcion 1 en el menu")
+                input("Presione una tecla para continuar...")
+            else :
+                system("cls")
+                mostrarLista(i)
+                input("Presione una tecla para continuar...")
+            
         case 9:
+            system("cls")
             print("Adios!")
             x=1
+        
+        case _:
+            system ("cls")
+            print("Ingrese una opcion valida!")
+            input("Presione una tecla para continuar...")
         
--- TADInvestigadores.py
+++ TADInvestigadores.py
@@ -1,64 +1,95 @@
-
+### FUNCIONES DE CARGA DE INVESTIGADORES ###
 investigador = ["","",0,0,0,""]
 
-def crearinvestigador():
+def crearInvestigador():
     #crea un investigador vacio
-    investigador = ["","",0,0,0,""]
+    investigador = ["","",0,"",0,""]
     return investigador
 
-def cargarinvestigador(investigador,nom,ape,leg,fecha,lab,area):
+def cargarInvestigador(investigador,nom,ape,leg,fecha,lab,area):
     #carga los datos de un nuevo investigador
     #[Nombre,Apellido,Legajo,Fecha de ingreso, Numero de lab, Area]
-    investigador[0]= nom
-    investigador[1]= ape
-    investigador[2]=leg
-    investigador[3]=fecha
-    investigador[4]=lab
-    investigador[5]=area
+    investigador[0] = nom
+    investigador[1] = ape
+    investigador[2] = leg
+    investigador[3] = fecha
+    investigador[4] = lab
+    investigador[5] = area
 
-def asignarinvestigador(investigador1,investigador2):
+def sumarInvestigador (lista, investigador):
+    lista.append(investigador)
+
+def asignarInvestigador(investigador1,investigador2):
     #copia los datos del investigador1 al investigador2
-    investigador1[0]= investigador2[0]
-    investigador1[1]= investigador2[1]
-    investigador1[2]= investigador2[2]
-    investigador1[3]= investigador2[3]
-    investigador1[4]= investigador2[4]
-    investigador1[5]= investigador2[5]
+    investigador1[0] = investigador2[0]
+    investigador1[1] = investigador2[1]
+    investigador1[2] = investigador2[2]
+    investigador1[3] = investigador2[3]
+    investigador1[4] = investigador2[4]
+    investigador1[5] = investigador2[5]
 
-def vernombre(investigador):
+### FUNCIONES DE VISTA ###
+
+def verNombre(investigador):
     #retorna el nombre del investigador
     return investigador[0]
 
-def verapellido(investigador):
+def verApellido(investigador):
     return investigador[1]
 
-def verlegajo(investigador):
+def verLegajo(investigador):
     return investigador[2]
 
-def verfecha(investigador):
+def verFecha(investigador):
     return investigador[3]
 
-def verlaboratorio(investigador):
+def verLaboratorio(investigador):
     return investigador[4]
 
-def verarea(investigador):
+def verArea(investigador):
     return investigador[5]
 
-def modnombre(investigador,nuevonomb):
+### FUNCIONES DE MODIFICACION ###
+
+def modNombre(investigador,nuevonomb):
     #modifica el nombre del investigador
     investigador[0]= nuevonomb
 
-def modapellido(investigador,nuevoapellido):
+def modApellido(investigador,nuevoapellido):
     investigador[1]= nuevoapellido
 
-def modlegajo(investigador,nuevolegajo):
+def modLegajo(investigador,nuevolegajo):
     investigador[2]= nuevolegajo
 
-def modfecha(investigador,nuevafecha):
+def modFecha(investigador,nuevafecha):
     investigador[3]= nuevafecha
 
-def modlab(investigador,nuevolab):
+def modLab(investigador,nuevolab):
     investigador[4] = nuevolab
 
-def modarea(investigador,nuevoarea):
+def modArea(investigador,nuevoarea):
     investigador[5]= nuevoarea
+
+### FUNCIONES DE VERIFICACION
+
+def esVacia(lista) :
+    return len(lista) == 0
+
+
+### FUNCIONES DE AYUDA ###
+
+def recorrerConParametro(listaInvestigadores, param1, param2):
+    
+    for i in range(len.listaInvestigadores): 
+        if (listaInvestigadores[i[param1]] == [param2]) :
+            return True
+        else :
+            return False
+        
+
+def check(listaInvestigadores):
+    for i in range(len(listaInvestigadores)) :
+        print (listaInvestigadores[i])
+
+def mostrarLista(listaInvestigadores):
+        print (listaInvestigadores)
