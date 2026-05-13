e3db71d978ab6d2c7e55e65642c7ac329c13e1be
Mosnterinhouse <lucas.cruz2003@hotmail.com>
2026-05-13 17:11:55 -0300

    Añadir archivo de funciones auxiliares. Modificacion de Aplicacion de Investigadores

---
+++ FuncionesAuxiliares.py
@@ -0,0 +1,82 @@
+from TADInvestigadores import *
+from datetime import date
+
+def verTodo(listaInvestigadores) :
+    for z in range(len(listaInvestigadores)):
+        print("Investigador N°: ", z)
+        print("Nombre:", verNombre(recuperarInvestigador(listaInvestigadores,z)))
+        print("Apellido:", verApellido(recuperarInvestigador(listaInvestigadores,z)))
+        print("Legajo:", verLegajo(recuperarInvestigador(listaInvestigadores,z)))
+        print("Fecha de Ingreso:", verFecha(recuperarInvestigador(listaInvestigadores,z)))
+        print("Numero de Laboratorio:", verLaboratorio(recuperarInvestigador(listaInvestigadores,z)))
+        print("Area:", verArea(recuperarInvestigador(listaInvestigadores,z)))
+
+def asignacionMasiva (yearInvestigadores, areaCambio, listaInvestigadores) :
+    for i in range(len(listaInvestigadores)) :
+        year = verFecha(recuperarInvestigador(listaInvestigadores,i))
+        if (year[-4:] == yearInvestigadores) :
+            modArea(recuperarInvestigador(listaInvestigadores,i),areaCambio)
+
+### FUNCIONES DE AYUDA ###
+def eliminarInvestigador(lista,inv):
+    lista.remove(inv)
+
+def recuperarInvestigador(lista,investigador):
+    return lista[investigador]
+
+### VERIFICACIONES DE INPUTS ###
+def verificarInputStr(mensajeInput) :
+        while True:
+            valor = input(mensajeInput)
+            if (valor.isdigit()) :
+                print ("Error: No se permiten Numeros. Ingrese caracteres validos")
+            elif len(valor.strip()) == 0 :
+                print ("Error: El campo no puede estar vacio")
+            else :
+                return valor.title()
+
+def verificarInputInt(mensajeInput) :
+        while True:
+            valor = input(mensajeInput)
+            if (valor.isalpha()) :
+                print ("Error: No se permiten Letras. Ingrese caracteres validos")
+            elif len(valor.strip()) == 0 :
+                print ("Error: El campo no puede estar vacio")
+            else :
+                return valor
+
+def verificarInputLegajo (mensajeInput, listaInvestigadores) :
+    while True :
+        legajo = verificarInputInt(mensajeInput)
+        if (esVacia(listaInvestigadores)) :
+            return legajo
+        else: 
+            for i in range (len(listaInvestigadores)) :
+                if (legajo==verLegajo(recuperarInvestigador(listaInvestigadores,i))) :
+                    print("El legajo ingresado ya existe!\nIntente nuevamente.")
+                else :
+                    return legajo
+                
+
+def verificarInputFecha(mensajeInput):
+    while True:
+        fecha = input(mensajeInput).strip()
+        if len(fecha) == 0 or len(fecha) < 10:
+            print("Error: Ingrese una fecha con el formato DD/MM/AAAA o DD-MM-AAAA.")
+            continue
+
+        fecha = fecha.replace("-", "/")
+
+        dia  = fecha[:2]
+        mes  = fecha[3:5]
+        year = fecha[-4:]
+
+        if not (dia.isdigit() and mes.isdigit() and year.isdigit()):
+            print("Error: La fecha debe contener solo numeros y separadores ( / o -).")
+            continue
+
+        try:
+            date(int(year), int(mes), int(dia))
+            return fecha 
+        except ValueError:
+            print("Error: La fecha ingresada no existe. Intente nuevamente.")
--- TADInvestigadores.py
+++ TADInvestigadores.py
@@ -1,5 +1,3 @@
-from datetime import date
-
 ### FUNCIONES DE CARGA DE INVESTIGADORES ###
 investigador = ["","",0,"",0,""]
 
@@ -51,7 +49,6 @@ def verLaboratorio(investigador):
 def verArea(investigador):
     return investigador[5]
 
-
 ### FUNCIONES DE MODIFICACION ###
 def modNombre(investigador,nuevonomb):
     #modifica el nombre del investigador
@@ -73,80 +70,5 @@ def modArea(investigador,nuevoarea):
     investigador[5]= nuevoarea
 
 ### FUNCIONES DE VERIFICACION
-
 def esVacia(lista) :
     return len(lista) == 0
-
-### FUNCIONES DE AYUDA ###
-def eliminarInvestigador(lista,inv):
-    lista.remove(inv)
-
-def recuperarInvestigador(lista,investigador):
-    return lista[investigador]
-
-def verificarInputFecha(mensajeInput):
-    while True:
-        fecha = input(mensajeInput).strip()
-        if len(fecha) == 0 or len(fecha) < 10:
-            print("Error: Ingrese una fecha con el formato DD/MM/AAAA o DD-MM-AAAA.")
-            continue
-
-        fecha = fecha.replace("-", "/")
-
-        dia  = fecha[:2]
-        mes  = fecha[3:5]
-        year = fecha[-4:]
-
-        if not (dia.isdigit() and mes.isdigit() and year.isdigit()):
-            print("Error: La fecha debe contener solo numeros y separadores ( / o -).")
-            continue
-
-        try:
-            date(int(year), int(mes), int(dia))
-            return fecha 
-        except ValueError:
-            print("Error: La fecha ingresada no existe. Intente nuevamente.")
-
-def asignacionMasiva (yearInvestigadores, areaCambio, listaInvestigadores) :
-    for i in range(len(listaInvestigadores)) :
-        year = verFecha(recuperarInvestigador(listaInvestigadores,i))
-        if (year[-4:] == yearInvestigadores) :
-            modArea(i,areaCambio)
-
-
-### VERIFICACIONES DE INPUTS ###
-
-def verificarInputStr(mensajeInput) :
-        while True:
-            valor = input(mensajeInput)
-            if (valor.isdigit()) :
-                print ("Error: No se permiten Numeros. Ingrese caracteres validos")
-            elif len(valor.strip()) == 0 :
-                print ("Error: El campo no puede estar vacio")
-            else :
-                return valor
-
-def verificarInputInt(mensajeInput) :
-        while True:
-            valor = input(mensajeInput)
-            if (valor.isalpha()) :
-                print ("Error: No se permiten Letras. Ingrese caracteres validos")
-            elif len(valor.strip()) == 0 :
-                print ("Error: El campo no puede estar vacio")
-            else :
-                return valor
-
-def verificarLegajo (mensajeInput, listaInvestigadores) :
-    while True :
-        legajo = verificarInputInt(mensajeInput)
-        if (esVacia(listaInvestigadores)) :
-            return legajo
-        else: 
-            for i in range (len(listaInvestigadores)) :
-                if (legajo==verLegajo(recuperarInvestigador(listaInvestigadores,i))) :
-                    print("El legajo ingresado ya existe!\nIntente nuevamente.")
-                else :
-                    return legajo
-                
-def niam () :
-    print ("ñam")
